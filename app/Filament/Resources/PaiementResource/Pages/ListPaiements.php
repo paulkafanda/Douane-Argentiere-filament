fe60658d8975b816f5d95c28bb10b3ebf881023f
kafandapaul <paulkaf84@gmail.com>
2024-08-31 20:19:04 +0200

    --feature add paeiment chart

--- app/Filament/Resources/PaiementResource/Pages/ListPaiements.php
+++ app/Filament/Resources/PaiementResource/Pages/ListPaiements.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PaiementResource\Pages;
 
 use App\Filament\Resources\PaiementResource;
+use App\Livewire\PaiementChart;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
@@ -14,6 +15,13 @@ class ListPaiements extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PaiementChart::class
         ];
     }
 }
